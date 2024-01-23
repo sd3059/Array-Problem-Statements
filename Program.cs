c595ddc2f147d76439bab8dc736073667bf1ad75
Shouvik <sd3059@srmist.edu.in>
2024-01-23 23:34:40 +0530

    [Shouvik] : UC1 Problem in finding the duplicate elements

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace Array_Problem_Statements
     {
         static void Main(string[] args)
         {
+            int[] a = { 1, 2, 2, 3, 4, 5, 6, 6, 6, 6, 7, 7, 7 };
+            for(int i = 0; i < a.Length; i++)
+            {
+                int count = 0;
+                for(int j=0;j<a.Length; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine($"{a[i]}. {count}");
+
+            }
         }
     }
 }
